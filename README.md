a824b8193c61d09db32b947e278a3a1ef3faca49
mohammadrobot <alshamsi.mohammad@gmail.com>
2024-12-03 06:27:54 +0000

    change the color of each robot

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Run the package
 
     ros2 launch barista_robot_description barista_xacro.launch.py
 
+    ros2 launch barista_robot_description barista_two_robots.launch.py
+
 Execute in Webshell 2 to run joint_state_publisher_gui
 
 	ros2 run joint_state_publisher_gui joint_state_publisher_gui
--- launch/barista_two_robots.launch.py
+++ launch/barista_two_robots.launch.py
@@ -4,13 +4,12 @@
 import random
 
 import os
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import ExecuteProcess, IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, LaunchConfiguration
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_prefix
+from ament_index_python.packages import get_package_prefix, get_package_share_directory
 
 import xacro
 
@@ -28,28 +27,9 @@ def generate_launch_description():
     # robot_desc_path = os.path.join(get_package_share_directory(package_description), "urdf", urdf_file)
     robot_desc_path = os.path.join(get_package_share_directory(package_description), "xacro", xacro_file)
 
-    # Robot State Publisher
-
-    # robot_state_publisher_node = Node(
-    #     package='robot_state_publisher',
-    #     executable='robot_state_publisher',
-    #     name='my_robot_state_publisher_node',
-    #     emulate_tty=True,
-    #     parameters=[{'use_sim_time': True, 'robot_description': Command(['xacro ', robot_desc_path])}],
-    #     output="screen"
-    # )
-
     # RVIZ Configuration
-    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'urdf_vis.rviz')
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'two_robot_vis.rviz')
 
-
-    rviz_node = Node(
-            package='rviz2',
-            executable='rviz2',
-            output='screen',
-            name='rviz_node',
-            parameters=[{'use_sim_time': True}],
-            arguments=['-d', rviz_config_dir])
 
     # Gazebo Configration 
     pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
@@ -100,9 +80,10 @@ def generate_launch_description():
     # entity_name = robot_base_name+"-"+str(int(random.random()*100000))
 
 
-    robot_name_1 = "barista_robot1"
-    robot_name_2 = "barista_robot2"
+    robot_name_1 = "rick"
+    robot_name_2 = "morty"
 
+# Robot State Publisher
     rsp_robot1 = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -122,6 +103,7 @@ def generate_launch_description():
                      'robot_description': Command(['xacro ', robot_desc_path, ' robot_name:=', robot_name_2])}],
         output="screen"
     )
+    
 
     # Spawn ROBOT Set Gazebo
 
@@ -139,6 +121,34 @@ def generate_launch_description():
                    '-topic', robot_name_2+'/robot_description']
     )
 
+    # Static transform world to odom
+    static_tf_pub_1 = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name="".join(['static_transform_publisher_', robot_name_1, '_odom']),
+        output='screen',
+        emulate_tty=True,
+        arguments=['0', '0', '0', '0', '0', '0', 'world', robot_name_1 + '/odom'] # parent, child
+    )
+
+    static_tf_pub_2 = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name="".join(['static_transform_publisher_', robot_name_2, '_odom']),
+        output='screen',
+        emulate_tty=True,
+        arguments=['0', '0', '0', '0', '0', '0', 'world', robot_name_2 + '/odom'] # parent, child
+    )
+
+    # lunch RVIZ
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        name='rviz_node',
+        parameters=[{'use_sim_time': True}],
+        arguments=['-d', rviz_config_dir])
+
     # create and return launch description object
     return LaunchDescription(
         [    
@@ -146,11 +156,12 @@ def generate_launch_description():
             default_value=[os.path.join(pkg_barista_robot_gazebo, 'worlds', 'barista_robot_empty.world'), ''],
             description='SDF world file'),        
             gazebo,
-            # robot_state_publisher_node,
             rviz_node,
             rsp_robot1,
             rsp_robot2,
             spawn_robot1,
-            spawn_robot2
+            spawn_robot2,
+            static_tf_pub_1,
+            static_tf_pub_2
         ]
     )
